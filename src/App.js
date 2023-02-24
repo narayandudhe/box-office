a3d5742b5cf37190c999f9d718e36e1ab3c90692
narayandudhe <narayandudhe1@gmail.com>
2023-02-25 04:16:47 +0530

    added react router and used it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is Homepage
+      </Route>
+      <Route exact path="/about">
+        this is about page
+      </Route>
+      <Route>this is 404 page</Route>
+    </Switch>
   );
 }
 
